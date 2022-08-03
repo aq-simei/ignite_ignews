205592c022a4462f1719d645a34d0b29ed294206
Simei <aq.simei@gmail.com>
2022-08-03 15:04:10 -0300

    test: implement tests for sign in button component

---
+++ src/components/signInButton/__tests__/notLoggedInButton.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "../../../utils/test-utils";
+import {NotLoggedInButton} from "../../signInButton/compose/notLoggedInButton/index";
+
+describe('NotLoggedInButton', () => {
+  const setup = () => {
+    render(<NotLoggedInButton />)
+  }
+
+  it('should render the GitHub Icon', () => {
+    setup();
+    const githubIcon = screen.getByTestId('github_icon');
+
+    expect(githubIcon).toBeInTheDocument();
+  });
+  
+  it('should render the Sign In message', () => {
+    setup();
+    const buttonText = screen.getByText('Sign In With Github');
+
+    expect(buttonText).toBeInTheDocument();
+  })
+})
